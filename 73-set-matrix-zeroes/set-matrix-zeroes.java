7c5eb15d0c53b104cbe3fb366cf4c475f8d3b991
SHUBHAM KUMAR <142377830+Shubham-kumarx@users.noreply.github.com>
2026-08-21 10:04:44 +0530

    Time: 1 ms (97.43%) | Memory: 47.3 MB (92.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -10,7 +10,6 @@ class Solution {
                 break;
             }
         }
-
         for(int j = 0; j < n; j++){
             if(matrix[0][j] == 0){
                 firstRowZero = true;
@@ -21,15 +20,16 @@ class Solution {
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(matrix[i][j] == 0){
-                    matrix[0][j] = 0;
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
             }
         }
         if(firstRowZero){
@@ -38,26 +38,5 @@ class Solution {
         if(firstColZero){
             for(int i = 0; i < m; i++) matrix[i][0] = 0;
         }
-
-        
-        /* BRUTE FORCE --------------------
-        boolean[] rows = new boolean[m];
-        boolean[] cols = new boolean[n];
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
-                }
-            }
-        }
-
-        for(int i = 0; i < m; i++ ){
-            for(int j = 0; j < n; j++){
-                if(rows[i] || cols[j]) matrix[i][j] = 0;
-            }
-        }  
-        */     
     }
 }
